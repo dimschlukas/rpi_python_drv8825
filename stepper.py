9e7d0671ea3544b21ec1424571a32acc8f2192a7
dimschlukas <dimschlukas@gmail.com>
2019-07-17 17:09:10 +0000

    fixed some bugs

--- stepper.py
+++ stepper.py
@@ -3,11 +3,15 @@ from time import sleep
 
 class StepperMotor:
     def __init__(self, enable_pin, step_pin, dir_pin, mode_pins, step_type):
-        self.GPIO.setmode(self.GPIO.BCM)
-        self.GPIO.setup(enable_pin, self.GPIO.OUT)
-        self.GPIO.setup(step_pin, self.GPIO.OUT)
-        self.GPIO.setup(dir_pin, self.GPIO.OUT)
-        self.GPIO.setup(mode_pins, self.GPIO.OUT)
+        self.enable_pin = enable_pin
+        self.step_pin = step_pin
+        self.dir_pin = dir_pin
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(enable_pin, GPIO.OUT)
+        GPIO.setup(step_pin, GPIO.OUT)
+        GPIO.setup(dir_pin, GPIO.OUT)
+        GPIO.setup(mode_pins, GPIO.OUT)
         resolution = {'Full':(0, 0, 0),
                     'Half':(1, 0, 0),
                     '1/4':(0, 1, 0),
@@ -20,16 +24,16 @@ class StepperMotor:
                     '1/8':8,
                     '1/16':16,
                     '1/32':32}
-        delay = .005/microsteps[step_type]
-        self.GPIO.output(mode_pins, resolution[step_type])
+        self.delay = .005/microsteps[step_type]
+        GPIO.output(mode_pins, resolution[step_type])
 
 
-    def run(self, steps, clockwise)
-        self.GPIO.output(self.enable_pin, self.GPIO.LOW)
-        self.GPIO.output(self.dir_pin, clockwise)
+    def run(self, steps, clockwise):
+        GPIO.output(self.enable_pin, GPIO.LOW)
+        GPIO.output(self.dir_pin, clockwise)
         for i in range(steps):
-            self.GPIO.output(self.step_pin, self.GPIO.HIGH)
-            self.sleep(self.delay)
-            self.GPIO.output(self.step_pin, self.GPIO.LOW)
-            self.sleep(self.delay)
-        self.GPIO.output(self.enable_pin, self.GPIO.HIGH)
+            GPIO.output(self.step_pin, GPIO.HIGH)
+            sleep(self.delay)
+            GPIO.output(self.step_pin, GPIO.LOW)
+            sleep(self.delay)
+        GPIO.output(self.enable_pin, GPIO.HIGH)
--- steppertest.py
+++ steppertest.py
@@ -1,4 +1,7 @@
 from stepper import StepperMotor
+from time import sleep
 
-StepperMotor.__init__(12, 23, 24, (14, 15, 18), '1/32')
-StepperMotor.run(200, True)
+motor = StepperMotor(12, 23, 24, (14, 15, 18), '1/32')
+motor.run(6400, True)
+sleep(0.5)
+motor.run(6400, False)
