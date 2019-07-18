555444e5ed9ac9a96d895062ec5450a718d8e7e1
Schmid Lukas <31358918+dimschlukas@users.noreply.github.com>
2019-07-18 21:38:43 +0200

    added comments

--- steppertest.py
+++ steppertest.py
@@ -1,7 +1,21 @@
 from stepper import StepperMotor
 from time import sleep
 
-motor = StepperMotor(12, 23, 24, (14, 15, 18), '1/32')
-motor.run(6400, True)
+# GPIO setup
+enable_pin = 12
+step_pin = 23
+dir_pin = 24
+mode_pins = (14, 15, 18)
+
+# Stepper motor setup
+step_type = '1/32'
+fullstep_delay = .005
+
+# create object
+motor = StepperMotor(enable_pin, step_pin, dir_pin, mode_pins, step_type, fullstep_delay)
+
+motor.enable(True)        # enables stepper driver
+motor.run(6400, True)     # run motor 6400 steps clowckwise
 sleep(0.5)
-motor.run(6400, False)
+motor.run(6400, False)    # run motor 6400 steps counterclockwise
+motor.enable(False)       # disable stepper driver
