7c5a897beb93cb5d226042c33a241105a28f53fc
dimschlukas <dimschlukas@gmail.com>
2019-07-19 11:39:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'rpi_python_drv8825',
   packages = ['rpi_python_drv8825'],
-  version = '0.1',
+  version = '0.2',
   license='gpl-3.0',
   description = 'Python library to controll a stepper motor with a DRV8825 driver connected to a Raspberry Pi',
   author = 'Lukas Schmid',
   author_email = 'dimschlukas@gmail.com',
   url = 'https://github.com/dimschlukas/rpi_python_drv8825',
-  download_url = 'https://github.com/dimschlukas/rpi_python_drv8825/archive/v0.1-alpha.tar.gz',
+  download_url = 'https://github.com/dimschlukas/rpi_python_drv8825/archive/v0.2-alpha.tar.gz',
   keywords = ['drv8825', 'control', 'steppermotor', 'stepper', 'motor', 'Raspberry', 'Pi'],
   install_requires=[
           'RPi.GPIO',
